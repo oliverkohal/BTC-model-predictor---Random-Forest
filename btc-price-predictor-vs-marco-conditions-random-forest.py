34f0d7e24959950a6f4b91947870fced1f7f06f1
Oliver Koh <140325819+oliverkohal@users.noreply.github.com>
2025-05-01 14:07:44 +0800

    Update btc-price-predictor-vs-marco-conditions-random-forest.py

--- btc-price-predictor-vs-marco-conditions-random-forest.py
+++ btc-price-predictor-vs-marco-conditions-random-forest.py
@@ -70,6 +70,7 @@ def main():
     st.title('BTC Price Predictor Against Macro Conditions')
     st.write("""This model demonstrates how Bitcoin's price dynamics have evolved beyond the traditional 4-year cycle narrative in 2025. 
     It highlights the increasing influence of macroeconomic factors on BTC's valuation.""")
+    st.write("""This also gives our users a powerful tool to simulate institutional BTC valuation models.""")
     st.write("""Use the sliders to explore various economic scenarios—from highly favorable to challenging conditions—and observe their significant impact on Bitcoin's predicted price movement.""")
     st.write("""Contains data from Mar-2015 to Mar-2025.""")
     
